de2902d835e0ff93893e64ec3f8b290c3a182f33
Jaime Sares <jaimesares@gmail.com>
2019-12-11 19:29:05 +0100

    wip

--- database/migrations/2019_12_11_181000_create_users_table.php
+++ database/migrations/2019_12_11_181000_create_users_table.php
@@ -12,8 +12,10 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->timestamp('last_login')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
