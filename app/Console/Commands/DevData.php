0bbec6c8a80f72ccfb4545ae6255a6734254465a
Jaime Sares <jaimesares@gmail.com>
2019-12-11 20:25:34 +0100

    wip devdata

--- app/Console/Commands/DevData.php
+++ app/Console/Commands/DevData.php
@@ -4,24 +4,29 @@ namespace App\Console\Commands;
 
 use App\Enum\Users\UserRole;
 use App\Enum\Users\UserStatus;
+use App\Models\Option;
 use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use RuntimeException;
 
 class DevData extends Command
 {
     protected $signature = 'sukycms:devdata';
 
-
     public function handle(): void
     {
-        if (app()->environment() === 'production') {
-            throw new RuntimeException('DevData can not run on production environment');
-        }
+        $this->canHandle();
 
         $this->callSilent('migrate:fresh');
 
-        factory(User::class)->create([
+        $this->createAdmin();
+        $this->createOptions();
+    }
+
+    private function createAdmin(): User
+    {
+        $user = factory(User::class)->create([
             'name' => 'Admin',
             'email' => 'sukycms@gmail.com',
             'password' => 'secret',
@@ -30,5 +35,31 @@ class DevData extends Command
         ]);
 
         $this->info('Admin user:');
+
+        return $user;
+    }
+
+    private function canHandle(): void
+    {
+        if (app()->environment() === 'production') {
+            throw new RuntimeException('DevData can not run on production environment');
+        }
+    }
+
+    private function createOptions(): Collection
+    {
+        $options = collect(config('options'));
+
+        $optionsInserted = collect([]);
+
+        $options->each(static function (array $value, string $key) use ($optionsInserted) {
+            $optionsInserted->push(Option::forceCreate([
+                'key' => $key,
+                'value' => $value['default'] ?? null,
+                'autoload' => $value['autoload'] ?? null,
+            ]));
+        });
+
+        return $optionsInserted;
     }
 }
---
+++ config/options.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'name' => [
+        'required' => true,
+        'autoload' => true,
+        'default' => 'SukyCMS'
+    ],
+    'email' => [
+        'required' => true,
+        'autoload' => true,
+        'default' => 'sukycms@gmail.com'
+    ],
+    'domain' => [
+        'required' => true,
+        'autoload' => true,
+        'default' => 'sukycms.test'
+    ],
+    'subdomain' => [
+        'required' => true,
+        'autoload' => true,
+        'default' => 'cms'
+    ],
+
+];
