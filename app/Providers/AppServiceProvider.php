4941d89d71b44bfaa069c0561028f769f113e49c
Jaime Sares <jaimesares@gmail.com>
2019-12-11 18:27:14 +0100

    add telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() !== 'production') {
             $this->app->register(IdeHelperServiceProvider::class);
             $this->app->register(DebugbarServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
         }
     }
 }
