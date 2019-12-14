db08face805c9ebc90161890acc913ec1b835bb0
Jaime Sares <jaimesares@gmail.com>
2019-12-14 11:51:00 +0100

    wip admin sidebar

--- app/Http/Controllers/Admin/AdminBaseController.php
+++ app/Http/Controllers/Admin/AdminBaseController.php
@@ -15,120 +15,120 @@ class AdminBaseController extends Controller
     {
         return [
             [
-                'title' => 'General',
+                'title' => __('admin.sidebar.general'),
                 'links' => [
                     [
-                        'title' => 'Escritorio',
+                        'title' => __('admin.sidebar.dashboard'),
                         'icon' => 'fas fa-columns fa-fw',
                         'url' => '',
                         'active' => 'admin'
                     ],
                     [
-                        'title' => 'Asociación',
+                        'title' => __('admin.sidebar.shelter'),
                         'icon' => 'fas fa-heart fa-fw',
                         'url' => '',
                     ],
                     [
-                        'title' => 'Estadísticas',
+                        'title' => __('admin.sidebar.stats'),
                         'icon' => 'fas fa-chart-bar fa-fw',
                         'url' => '',
                     ],
                     [
-                        'title' => 'Calendario',
+                        'title' => __('admin.sidebar.calendar'),
                         'icon' => 'fas fa-calendar fa-fw',
                         'url' => '',
                     ],
                 ],
             ],
             [
-                'title' => 'Gestión',
+                'title' => __('admin.sidebar.management'),
                 'links' => [
                     [
-                        'title' => 'Animales',
+                        'title' => __('admin.sidebar.animals'),
                         'icon' => 'fas fa-paw fa-fw',
                         'url' => '#',
                         'submenu' => [
                             [
-                                'title' => 'Listado',
+                                'title' => __('admin.sidebar.list'),
                                 'url' => '',
                             ],
                             [
-                                'title' => 'Crear ficha',
+                                'title' => __('admin.sidebar.add_animal'),
                                 'url' => '',
                             ],
                         ],
                     ],
                     [
-                        'title' => 'Artículos',
+                        'title' => __('admin.sidebar.posts'),
                         'icon' => 'fas fa-file-alt fa-fw',
                         'url' => '#',
                         'submenu' => [
                             [
-                                'title' => 'Listado',
+                                'title' => __('admin.sidebar.list'),
                                 'url' => '',
                             ],
                             [
-                                'title' => 'Crear ficha',
+                                'title' => __('admin.sidebar.publish'),
                                 'url' => '',
                             ],
                         ],
                     ],
                     [
-                        'title' => 'Páginas',
+                        'title' => __('admin.sidebar.pages'),
                         'icon' => 'fas fa-file-powerpoint fa-fw',
                         'url' => '#',
                         'submenu' => [
                             [
-                                'title' => 'Listado',
+                                'title' => __('admin.sidebar.list'),
                                 'url' => '',
                             ],
                             [
-                                'title' => 'Crear ficha',
+                                'title' => __('admin.sidebar.new'),
                                 'url' => '',
                             ],
                         ],
                     ],
                     [
-                        'title' => 'Personas',
+                        'title' => __('admin.sidebar.people'),
                         'icon' => 'fas fa-users fa-fw',
                         'url' => '#',
                         'submenu' => [
                             [
-                                'title' => 'Listado',
+                                'title' => __('admin.sidebar.list'),
                                 'url' => '',
                             ],
                             [
-                                'title' => 'Crear ficha',
+                                'title' => __('admin.sidebar.new'),
                                 'url' => '',
                             ],
                         ],
                     ],
                     [
-                        'title' => 'Formularios',
+                        'title' => __('admin.sidebar.forms'),
                         'icon' => 'fas fa-clipboard-list fa-fw',
                         'url' => '#',
                         'submenu' => [
                             [
-                                'title' => 'Listado',
+                                'title' => __('admin.sidebar.list'),
                                 'url' => '',
                             ],
                             [
-                                'title' => 'Crear ficha',
+                                'title' => __('admin.sidebar.new'),
                                 'url' => '',
                             ],
                         ],
                     ],
                     [
-                        'title' => 'Archivos',
+                        'title' => __('admin.sidebar.files'),
                         'icon' => 'fas fa-cloud-upload-alt fa-fw',
                         'url' => '#',
                         'submenu' => [
                             [
-                                'title' => 'Listado',
+                                'title' => __('admin.sidebar.list'),
                                 'url' => '',
                             ],
                             [
-                                'title' => 'Crear ficha',
+                                'title' => __('admin.sidebar.new'),
                                 'url' => '',
                             ],
                         ],
@@ -136,20 +136,20 @@ class AdminBaseController extends Controller
                 ],
             ],
             [
-                'title' => 'Soporte',
+                'title' => __('admin.sidebar.support'),
                 'links' => [
                     [
-                        'title' => 'Documentación',
+                        'title' => __('admin.sidebar.documentation'),
                         'icon' => 'fas fa-file-alt fa-fw',
                         'url' => '',
                     ],
                     [
-                        'title' => 'Contacto',
+                        'title' => __('admin.sidebar.contact'),
                         'icon' => 'fas fa-user fa-fw',
                         'url' => '',
                     ],
                     [
-                        'title' => 'Preguntas frecuentes',
+                        'title' => __('admin.sidebar.faq'),
                         'icon' => 'fas fa-question-circle fa-fw',
                         'url' => '',
                     ],
---
+++ resources/lang/es/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'sidebar' => [
+        'general' => 'General',
+        'dashboard' => 'Escritorio',
+        'shelter' => 'Asociación',
+        'stats' => 'Estadísticas',
+        'calendar' => 'Calendario',
+
+        'management' => 'Gestión',
+        'animals' => 'Animales',
+        'posts' => 'Artículos',
+        'pages' => 'Páginas',
+        'people' => 'Personas',
+        'forms' => 'Formularios',
+        'files' => 'Archivos',
+
+        'support' => 'Soporte',
+        'documentation' => 'Documentación',
+        'contact' => 'Contacto',
+        'faq' => 'Preguntas frecuentes',
+
+        'list' => 'Listado',
+        'publish' => 'Publicar',
+        'new' => 'Nuevo',
+        'add_animal' => 'Crear ficha',
+    ],
+
+];
