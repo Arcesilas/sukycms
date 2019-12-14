254183d8260885a1a404c0e19f08a53cfe915caf
Jaime Sares <jaimesares@gmail.com>
2019-12-14 16:58:18 +0100

    return option instance if not key

--- app/Support/functions.php
+++ app/Support/functions.php
@@ -1,5 +1,9 @@
 <?php
 
-function option(string $key, $default = null) {
+function option(string $key = null, $default = null) {
+    if (! $key) {
+        return app('options');
+    }
+
     return app('options')[$key] ?? $default;
 }
