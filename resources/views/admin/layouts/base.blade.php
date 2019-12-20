f7988f93f2838239f9e3a9685b43d33f1d04e32a
Jaime Sares <jaimesares@gmail.com>
2019-12-21 00:08:27 +0100

    favicon

--- resources/views/admin/layouts/base.blade.php
+++ resources/views/admin/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ option('name') }}</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
 
     <link rel="stylesheet" href="{{ mix('build/admin.css') }}">
     @stack('styles')
