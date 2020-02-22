6c47883d7b861fffe72596eec57bcfd86cbd5a8e
Jaime Sares <jaimesares@gmail.com>
2020-02-22 21:13:39 +0100

    wip

--- app/Filters/AnimalFilters.php
+++ app/Filters/AnimalFilters.php
@@ -4,7 +4,17 @@ namespace App\Filters;
 
 class AnimalFilters extends Filter
 {
+    public array $filters = [
+        'q',
+        'species',
+    ];
+
     protected array $searchFields = [
         'name',
     ];
+
+    public function species(int $species_id)
+    {
+        $this->builder->where('species_id', $species_id);
+    }
 }
