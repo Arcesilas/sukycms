f7675626ef2c5a8dbde05429e9bc98376dccc07f
Jaime Sares <jaimesares@gmail.com>
2020-02-29 12:36:29 +0100

    wip laravel file manager

--- resources/views/components/forms/fields/textarea.blade.php
+++ resources/views/components/forms/fields/textarea.blade.php
@@ -11,7 +11,7 @@
         </label>
     @endif
 
-    <textarea type="text" name="{{ $field->name }}" id="{{ $field->id }}" class="ckeditor"
+    <textarea type="text" name="{{ $field->name }}" id="{{ $field->id }}" class="lckeditor"
     @foreach ($field->options as $key => $value)
         {{ $key }}="{{ $value }}"
     @endforeach
@@ -26,9 +26,16 @@
 @endif
 
 @push('scripts')
-    <script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+    <script src="https://cdn.ckeditor.com/4.13.0/standard/ckeditor.js"></script>
     <script>
-        CKEDITOR.config.height = 300;
-        CKEDITOR.config.language = 'es';
+        let options = {
+            height: 300,
+            language: 'es',
+            filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+            filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+            filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+            filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token=',
+        };
+        CKEDITOR.replace('{{ $field->id }}', options);
     </script>
 @endpush
