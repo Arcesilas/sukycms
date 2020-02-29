9a867ac075ecc9cb93100f66dd198fac4027769b
Jaime Sares <jaimesares@gmail.com>
2020-02-29 20:01:18 +0100

    wip create post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Filters\Filterable;
 use App\Forms\Admin\PostForm;
 use App\Scopes\UserScope;
+use Str;
 
 class Post extends BaseModel
 {
@@ -19,6 +20,11 @@ class Post extends BaseModel
     public static function boot(): void
     {
         parent::boot();
+
+        static::creating(function (Post $post) {
+            $post->slug = Str::slug($post->title);
+            $post->user_id = auth()->user()->id;
+        });
 
         static::addGlobalScope(new UserScope());
     }
