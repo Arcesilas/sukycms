d8f573780edd7436ea8dc6b00c2ef80fa0ca86d9
Jaime Sares <jaimesares@gmail.com>
2020-03-01 00:51:21 +0100

    add laravel-ui

---
+++ resources/lang/es/pages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'create' => [
+        'title' => 'Crear página',
+        'description' => [
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.',
+        ],
+    ],
+
+];
--- resources/views/admin/layouts/crud/create.blade.php
+++ resources/views/admin/layouts/crud/create.blade.php
@@ -2,12 +2,18 @@
 
 @section('content')
 
+    <h4 class="text-xl mb-4">{{ __($transNamespace.'.create.title') }}</h4>
+    @if (is_iterable(__($transNamespace.'.create.description')))
+        <div class="text-gray-600">
+            @foreach (__($transNamespace.'.create.description') ?? [] as $description)
+                <p class="w-2/3 {{ ! $loop->last ? 'mb-4' : '' }}">{!! $description !!}</p>
+            @endforeach
+        </div>
+    @endif
+
     {!! $form->renderStart() !!}
 
-    <div class="card">
-        <div class="card-title flex flex-row justify-between">
-            <h3>{{ __($transNamespace.'.create.title') }}</h3>
-        </div>
+    <div class="card mt-8">
         <div class="card-body">
             <hr class="mt-8 mb-12">
 
