e0069e7935f2cc3a4c347a833ef2e5eeea94c93e
Jaime Sares <jaimesares@gmail.com>
2020-03-13 11:15:20 +0100

    wip dont destroy last

--- app/Http/Controllers/Admin/AnimalLocationController.php
+++ app/Http/Controllers/Admin/AnimalLocationController.php
@@ -5,15 +5,14 @@ namespace App\Http\Controllers\Admin;
 use App\Filters\AnimalLocationFilters;
 use App\Models\AnimalLocation;
 use App\Support\Crud\Crud;
+use App\Support\Crud\DontDestroyLast;
 use App\Support\Crud\Fields\Text;
 use App\Support\Orderable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Collection;
 
 class AnimalLocationController extends AdminBaseController
 {
-    use Crud, Orderable;
+    use Crud, Orderable, DontDestroyLast;
 
     protected string $model = AnimalLocation::class;
 
@@ -25,36 +24,6 @@ class AnimalLocationController extends AdminBaseController
             ->filter(app(AnimalLocationFilters::class))
             ->orderBy('order')
             ->get();
-    }
-
-    public function viewShare(): array
-    {
-        return [
-            'locations' => AnimalLocation::withCount('animals')
-                ->orderBy('order')
-                ->get(),
-        ];
-    }
-
-    public function preDestroy(Model $model): bool
-    {
-        if ($model->animals->count()) {
-            $newModel = AnimalLocation::findOrFail(request()->get('model_id'));
-
-            $model->animals()->update([
-                'location_id' => $newModel->id,
-            ]);
-        }
-
-        if (AnimalLocation::count() === 1) {
-            flash(
-                __($this->transNamespace.'.destroy.error'),
-            )->show();
-    
-            return false;
-        }
-
-        return true;
     }
 
     public function fields(): array
